09dbd01245edcdf01ea5ba3c81949f73ede84ddc
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2021-10-26 09:44:15 -0700

    added paddle and its function

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const y = canvas.height - 30;
 const dx = 2;
 const dy = -2;
 const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
+const paddleX = (canvas.width - paddleWidth) / 2;
+const rightPressed = false;
+const leftPressed = false;
 
 function drawBall() {
   ctx.beginPath();
@@ -15,10 +20,19 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   // drawing code
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   // eslint-disable-next-line no-const-assign
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
@@ -28,5 +42,35 @@ function draw() {
   }
   x += dx;
   y += dy;
+
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth;
+    }
+  } else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
+  }
+}
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
 }
 setInterval(draw, 10);
