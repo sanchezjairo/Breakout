eff8ed695af2225d2f835117853c0e8760386d55
Jairo Sanchez <69741616+sanchezjairo@users.noreply.github.com>
2021-10-28 16:49:51 -0700

    Added collision detection

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ for (const c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   // eslint-disable-next-line no-plusplus
   for (const r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
@@ -51,15 +51,17 @@ function drawBricks() {
   for (const c = 0; c < brickColumnCount; c++) {
     // eslint-disable-next-line no-plusplus
     for (const r = 0; r < brickRowCount; r++) {
-      const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-      const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(0, 0, brickWidth, brickHeight);
-      ctx.fillStyle = '#0095DD';
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status === 1 ) {
+        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(0, 0, brickWidth, brickHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -70,6 +72,7 @@ function draw() {
   drawBricks();
   drawBall();
   drawPaddle();
+  collisionDetection()
   // eslint-disable-next-line no-const-assign
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
@@ -116,5 +119,21 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
     leftPressed = false;
+  }
+}
+
+function collisionDetection() {
+  // eslint-disable-next-line no-plusplus
+  for (const c = 0; c < brickColumnCount; c++) {
+    // eslint-disable-next-line no-plusplus
+    for (const r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status === 1) {
+        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
   }
 }
